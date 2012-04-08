449814f50b7807c7251f59f413d5ed0cb278aa9f
Arnaud Lemaire <dev@norad.fr>
2012-04-08 22:54:29 +0200

    up

---
+++ server/service/src/main/java/net/awired/housecream/server/core/service/HccResource.java
@@ -0,0 +1,9 @@
+package net.awired.housecream.server.core.service;
+
+import javax.ws.rs.Path;
+import net.awired.housecream.client.common.resource.server.HccNotifyResource;
+
+@Path("/")
+public interface HccResource extends HccNotifyResource {
+
+}
