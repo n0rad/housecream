3e5877b9101e671febb990824c19d759fdde9dbd
Arnaud Lemaire <dev@norad.fr>
2012-10-05 00:23:44 +0200

    cleanup

--- web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
+++ web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
@@ -1,14 +1,7 @@
 define(['jquery', 'underscore', 'ajsl/event', 'text!./ImageMap.html', 'text!./ImageMapRow.html', 'js!./imgmap.js', 'js!./lang_en.js'], 
 function($, _, event, ImageMapTpl, ImageMapRow) {
 	
-	
-
-	/** GLOBALS SECTION ***********************************************************/
-
-	var myimgmap, props, outputmode;
-
-	/** FUNCTION SECTION **********************************************************/
-
+	var myimgmap, props;
 
 	/**
 	 *	Handles mouseover on props row.
@@ -72,20 +65,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 		props[id].style.background = '#e7e7e7';
 	}
 
-//	/**
-//	 *	Handles delete keypress when focus is on the leading checkbox/radio.
-//	 *	@author	adam 
-//	 */
-//	function gui_cb_keydown(e) {
-//		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-//		var key = (myimgmap.isMSIE) ? event.keyCode : e.keyCode;
-//		//alert(key);
-//		if (key == 46) {
-//			//delete pressed
-//			myimgmap.removeArea(myimgmap.currentid);
-//		}
-//	}
-
 	/**
 	 *	Unchecks all checboxes/radios.
 	 */
@@ -94,46 +73,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 			if (props[i]) {
 				document.getElementById('img_active_'+i).checked = false;
 			}
-		}
-	}
-
-	/**
-	 *	Handles arrow keys on img_coords input field.
-	 *	Changes the coordinate values by +/- 1 and updates the corresponding canvas area.
-	 *	@author	adam
-	 *	@date	25-09-2007 17:12:43
-	 */
-	function gui_coords_keydown(e) {
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		var key = (myimgmap.isMSIE || myimgmap.isOpera) ? event.keyCode : e.keyCode;
-		var obj = (myimgmap.isMSIE || myimgmap.isOpera) ? window.event.srcElement : e.originalTarget;
-		//obj is the input field
-		//this.log(key);
-		//this.log(obj);
-		if (key == 40 || key == 38) {
-			//down or up pressed
-			//get the coords
-			var coords = obj.value.split(',');
-			var s = getSelectionStart(obj);//helper function
-			var j = 0;
-			for (var i=0, le = coords.length; i<le; i++) {
-				j+=coords[i].length;
-				if (j > s) {
-					//this is the coord we want
-					if (key == 40 && coords[i] > 0) {coords[i]--;}
-					if (key == 38) {coords[i]++;}
-					break;
-				}
-				//jump one more because of comma
-				j++;
-			}
-			obj.value = coords.join(',');
-			if (obj.value != myimgmap.areas[obj.parentNode.aid].lastInput) {
-				myimgmap._recalculate(obj.parentNode.aid, obj.value);//contains repaint
-			}
-			//set cursor back to its original position
-			setSelectionRange(obj, s);
-			return true;
 		}
 	}
 
@@ -218,64 +157,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 		}
 	}
 
-//	/**
-//	 *	Called from imgmap when a new area is added.
-//	 */
-//	function gui_addArea(id) {
-//		//var id = props.length;
-//		//id = 1;
-//		props[id] = document.createElement('DIV');
-//		document.getElementById('form_container').appendChild(props[id]);
-//
-//		props[id].id        = 'img_area_' + id;
-//		props[id].aid       = id;
-//		props[id].className = 'img_area';
-//		//hook ROW event handlers
-//		myimgmap.addEvent(props[id], 'mouseover', gui_row_mouseover);
-//		myimgmap.addEvent(props[id], 'mouseout',  gui_row_mouseout);
-//		myimgmap.addEvent(props[id], 'click',     gui_row_click);
-//		var temp = '<input type="text"  name="img_id" class="img_id" value="' + id + '" readonly="1"/>';
-//		//temp+= '<input type="checkbox" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
-//		//could be checkbox in the future
-//		temp+= '<input type="radio" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
-//		temp+= '<select name="img_shape" class="img_shape">';
-//		temp+= '<option value="rect">rectangle</option>';
-//		if (document.getElementById('dd_output').value != 'css') {
-//			temp+= '<option value="circle">circle</option>';
-//			temp+= '<option value="poly">polygon</option>';
-//			temp+= '<option value="bezier1">bezier</option>';
-//		}
-//		temp+= '</select>';
-//		temp+= 'Coords: <input type="text" name="img_coords" class="img_coords" value="">';
-////		temp+= 'Href: <input type="text" name="img_href" class="img_href" value="">';
-////		temp+= 'Alt: <input type="text" name="img_alt" class="img_alt" value="">';
-////		temp+= 'Target: <select name="img_target" class="img_target">';
-//		temp+= '<option value=""  >&lt;not set&gt;</option>';
-//		temp+= '<option value="_self"  >this window</option>';
-//		temp+= '<option value="_blank" >new window</option>';
-//		temp+= '<option value="_top"   >top window</option>';
-//		temp+= '</select>';
-//		props[id].innerHTML = temp;
-//		//hook more event handlers to individual inputs
-//		
-//		myimgmap.addEvent(props[id].getElementsByTagName('input')[1],  'keydown', gui_cb_keydown);
-//		myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'keydown', gui_coords_keydown);
-//		myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'change', gui_input_change);
-////		myimgmap.addEvent(props[id].getElementsByTagName('input')[3],  'change', gui_input_change);
-////		myimgmap.addEvent(props[id].getElementsByTagName('input')[4],  'change', gui_input_change);
-//		myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_input_change);
-////		myimgmap.addEvent(props[id].getElementsByTagName('select')[1], 'change', gui_input_change);
-//		if (myimgmap.isSafari) {
-//			//need these for safari
-//			myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_row_click);
-////			myimgmap.addEvent(props[id].getElementsByTagName('select')[1], 'change', gui_row_click);
-//		}
-//		//set shape as nextshape if set
-//		if (myimgmap.nextShape) {props[id].getElementsByTagName('select')[0].value = myimgmap.nextShape;}
-//		//alert(this.props[id].parentNode.innerHTML);
-//		gui_row_select(id, true);
-//	}
-
 	/**
 	 *	Called from imgmap when an area was removed.
 	 */
@@ -347,30 +228,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 		document.getElementById('html_container').focus();
 	}
 
-//	/**
-//	 *	Change the labeling mode directly in imgmap config then repaint all areas.
-//	 */
-//	function changelabeling(obj) {
-//		myimgmap.config.label = obj.value;
-//		myimgmap._repaintAll();
-//	}
-
-//	function gui_selectArea(obj) {
-//		gui_row_select(obj.aid, true, false);
-//	}
-
-//	function gui_loadImage(src) {
-//		//reset zoom dropdown
-//		document.getElementById('dd_zoom').value = '1';
-//		var pic = document.getElementById('pic_container').getElementsByTagName('img')[0];
-//		if (typeof pic != 'undefined') {
-//			//delete already existing pic
-//			pic.parentNode.removeChild(pic);
-//			delete myimgmap.pic;
-//		}
-//		myimgmap.loadImage(src);
-//	}
-
 	function gui_outputChanged() {
 		var temp, i;
 		var output = document.getElementById('dd_output').value;
@@ -378,7 +235,7 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 		document.getElementById('output_help').innerHTML = temp;
 		//this will reload areas and sets dropdown restrictions
 		myimgmap.setMapHTML(myimgmap.getMapHTML());
-		outputmode = output;
+//		outputmode = output;
 		return true;
 	}
 
@@ -462,32 +319,19 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					temp+= '<option value="bezier1">bezier</option>';
 					temp+= '</select>';
 					temp+= 'Coords: <input type="text" name="img_coords" class="img_coords" value="">';
-//					temp+= 'Href: <input type="text" name="img_href" class="img_href" value="">';
-//					temp+= 'Alt: <input type="text" name="img_alt" class="img_alt" value="">';
-//					temp+= 'Target: <select name="img_target" class="img_target">';
-//					temp+= '<option value=""  >&lt;not set&gt;</option>';
-//					temp+= '<option value="_self"  >this window</option>';
-//					temp+= '<option value="_blank" >new window</option>';
-//					temp+= '<option value="_top"   >top window</option>';
-//					temp+= '</select>';
 					props[id].innerHTML = temp;
 					//hook more event handlers to individual inputs
 					
-//					myimgmap.addEvent(props[id].getElementsByTagName('input')[1],  'keydown', gui_cb_keydown);
-//					myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'keydown', gui_coords_keydown);
 					myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'change', gui_input_change);
 //					myimgmap.addEvent(props[id].getElementsByTagName('input')[3],  'change', gui_input_change);
 //					myimgmap.addEvent(props[id].getElementsByTagName('input')[4],  'change', gui_input_change);
 					myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_input_change);
-//					myimgmap.addEvent(props[id].getElementsByTagName('select')[1], 'change', gui_input_change);
 					if (myimgmap.isSafari) {
 						//need these for safari
 						myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_row_click);
-//						myimgmap.addEvent(props[id].getElementsByTagName('select')[1], 'change', gui_row_click);
 					}
 					//set shape as nextshape if set
 					if (myimgmap.nextShape) {props[id].getElementsByTagName('select')[0].value = myimgmap.nextShape;}
-					//alert(this.props[id].parentNode.innerHTML);
 					gui_row_select(id, true);					
 				},
 				onRemoveArea : function(id)  {
@@ -528,7 +372,7 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 				myimgmap.strings = imgmapStrings;
 
 				props = [];
-				outputmode = 'imgmap';
+//				outputmode = 'imgmap';
 //				gui_outputChanged();
 
 //				myimgmap.addEvent(document.getElementById('html_container'), 'blur',  gui_htmlBlur);
