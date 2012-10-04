e538578d0d3ecfcd8441b51b1afb87e77ec619fa
Arnaud Lemaire <dev@norad.fr>
2012-10-05 00:54:36 +0200

    cleanup

--- web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
+++ web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
@@ -1,7 +1,8 @@
 define(['jquery', 'underscore', 'ajsl/event', 'text!./ImageMap.html', 'text!./ImageMapRow.html', 'js!./imgmap.js', 'js!./lang_en.js'], 
 function($, _, event, ImageMapTpl, ImageMapRow) {
 	
-	var myimgmap, props;
+	var myimgmap;
+	var props = [];
 
 	function gui_row_mouseover(e) {
 		if (myimgmap.is_drawing) {return;}//exit if in drawing state
@@ -56,11 +57,8 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 	function gui_input_change(e) {
 		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
 		if (myimgmap.is_drawing) {return;}//exit if drawing
-		//console.log('blur');
 		var obj = (myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
-		//console.log(obj);
 		var id = obj.parentNode.aid;
-		//console.log(this.areas[id]);
 		if (obj.name == 'img_href')        {myimgmap.areas[id].ahref   = obj.value;}
 		else if (obj.name == 'img_alt')    {myimgmap.areas[id].aalt    = obj.value;}
 		else if (obj.name == 'img_title')  {myimgmap.areas[id].atitle  = obj.value;}
@@ -93,29 +91,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 		}
 	}
 
-	function gui_areaChanged(area) {
-		var id = area.aid;
-		if (props[id]) {
-			if (area.shape)  {props[id].getElementsByTagName('select')[0].value = area.shape;}
-			if (area.lastInput) {props[id].getElementsByTagName('input')[2].value  = area.lastInput;}
-			if (area.ahref)   {props[id].getElementsByTagName('input')[3].value  = area.ahref;}
-			if (area.aalt)    {props[id].getElementsByTagName('input')[4].value  = area.aalt;}
-			if (area.atarget) {props[id].getElementsByTagName('select')[1].value = area.atarget;}
-		}
-	}
-
-	function gui_outputChanged() {
-		var temp, i;
-		var output = document.getElementById('dd_output').value;
-		temp = 'This is the generated image map HTML code. ';
-		document.getElementById('output_help').innerHTML = temp;
-		//this will reload areas and sets dropdown restrictions
-		myimgmap.setMapHTML(myimgmap.getMapHTML());
-//		outputmode = output;
-		return true;
-	}
-	
-	
 	function ImageMap(context) {
 		var self = this;
 		this.context = context;
@@ -158,6 +133,9 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					document.getElementById('html_container').value = str;
 				},
 				onAddArea : function(id)  {
+					//this will reload areas and sets dropdown restrictions
+//					myimgmap.setMapHTML(myimgmap.getMapHTML());
+					
 //					myimgmap.setMapHTML(elem.value);
 //					myimgmap.removeArea
 //					myimgmap.areas[obj.parentNode.aid].lastInput)
@@ -185,7 +163,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					myimgmap.addEvent(props[id], 'click',     gui_row_click);
 					var temp = '<input type="text"  name="img_id" class="img_id input-mini" value="' + id + '" readonly="1"/>';
 					//temp+= '<input type="checkbox" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
-					//could be checkbox in the future
 					temp+= '<input type="radio" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
 					temp+= '<select name="img_shape" class="img_shape input-small">';
 					temp+= '<option value="rect">rectangle</option>';
@@ -198,8 +175,6 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					//hook more event handlers to individual inputs
 					
 					myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'change', gui_input_change);
-//					myimgmap.addEvent(props[id].getElementsByTagName('input')[3],  'change', gui_input_change);
-//					myimgmap.addEvent(props[id].getElementsByTagName('input')[4],  'change', gui_input_change);
 					myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_input_change);
 					if (myimgmap.isSafari) {
 						//need these for safari
@@ -217,8 +192,14 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					gui_row_select(lastid, true);
 					myimgmap.currentid = lastid;
 				},
-				onAreaChanged : function(obj) {
-//					gui_areaChanged(obj);
+				onAreaChanged : function(area) {
+					var id = area.aid;
+					if (area.shape) {
+						props[id].getElementsByTagName('select')[0].value = area.shape;
+					}
+					if (area.lastInput) {
+						props[id].getElementsByTagName('input')[2].value  = area.lastInput;
+					}
 				},
 				onSelectArea : function(obj) {
 					gui_row_select(obj.aid, true, false);
@@ -237,26 +218,13 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 
 				myimgmap = new imgmap({
 					mode : "editor",
-					custom_callbacks : {
-						'onStatusMessage' : this.imgMapCallbacks.onStatusMessage,
-						'onHtmlChanged'   : this.imgMapCallbacks.onHtmlChanged,
-//						'onModeChanged'   : this.imgMapCallbacks.onModeChanged,
-						'onAddArea'       : this.imgMapCallbacks.onAddArea,
-						'onRemoveArea'    : this.imgMapCallbacks.onRemoveArea,
-						'onAreaChanged'   : function(obj) {gui_areaChanged(obj);},
-						'onSelectArea'    : this.imgMapCallbacks.onSelectArea
-					},
-					pic_container: document.getElementById('pic_container'),
+					custom_callbacks : this.imgMapCallbacks,
+					pic_container: $('#pic_container', this.context)[0],
 					bounding_box : false
 				});
 				myimgmap.strings = imgmapStrings;
 
-				props = [];
-//				outputmode = 'imgmap';
-//				gui_outputChanged();
-
-				myimgmap.loadImage('http://localhost:8080/hcw/hcw/view/admin/imagemap/example1_files/sample1.jpg');
-				
+				myimgmap.loadImage('http://localhost:8080/hcw/hcw/view/admin/imagemap/example1_files/sample1.jpg');				
 			}
 	};
 	
