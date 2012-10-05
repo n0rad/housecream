56d94153f3df2878c5777a01fc5e40eb179f97d2
Arnaud Lemaire <dev@norad.fr>
2012-10-05 10:36:03 +0200

    first version with full context for image map

--- web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
+++ web/src/main/webapp/hcw/view/admin/imagemap/ImageMap.js
@@ -1,99 +1,110 @@
 define(['jquery', 'underscore', 'ajsl/event', 'text!./ImageMap.html', 'text!./ImageMapRow.html', 'js!./imgmap.js', 'js!./lang_en.js'], 
 function($, _, event, ImageMapTpl, ImageMapRow) {
-	
-	var myimgmap;
-	var props = [];
 
-	function gui_row_mouseover(e) {
-		if (myimgmap.is_drawing) {return;}//exit if in drawing state
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		var obj = (myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
-		if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
-		//console.log(obj.aid);
-		myimgmap.highlightArea(obj.aid);
-	}
+//this will reload areas and sets dropdown restrictions
+//	self.myimgmap.setMapHTML(self.myimgmap.getMapHTML());
 
-	function gui_row_mouseout(e) {
-		if (myimgmap.is_drawing) {return;}//exit if in drawing state
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		var obj = (myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
-		if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
-		myimgmap.blurArea(obj.aid);
-	}
-
-	function gui_row_click(e) {
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		var obj = (myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
-		if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
-		gui_row_select(obj.aid, false, false);
-		myimgmap.currentid = obj.aid;
-	}
-
-	function gui_row_select(id, setfocus, multiple) {
-		if (myimgmap.is_drawing) {return;}//exit if in drawing state
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		if (!document.getElementById('img_active_'+id)) {return;}
-
-		for (var i = 0; i < props.length; i++) {
-			if (props[i]) {
-				document.getElementById('img_active_'+i).checked = false;
-			}
-		}
-
-		document.getElementById('img_active_'+id).checked = 1;
-		if (setfocus) {
-			document.getElementById('img_active_'+id).focus();
-		}
-		//remove all background styles
-		for (var i = 0; i < props.length; i++) {
-			if (props[i]) {
-				props[i].style.background = '';
-			}
-		}
-		//put highlight on actual props row
-		props[id].style.background = '#e7e7e7';
-	}
-
-	function gui_input_change(e) {
-		if (myimgmap.viewmode === 1) {return;}//exit if preview mode
-		if (myimgmap.is_drawing) {return;}//exit if drawing
-		var obj = (myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
-		var id = obj.parentNode.aid;
-		if (obj.name == 'img_href')        {myimgmap.areas[id].ahref   = obj.value;}
-		else if (obj.name == 'img_alt')    {myimgmap.areas[id].aalt    = obj.value;}
-		else if (obj.name == 'img_title')  {myimgmap.areas[id].atitle  = obj.value;}
-		else if (obj.name == 'img_target') {myimgmap.areas[id].atarget = obj.value;}
-		else if (obj.name == 'img_shape') {
-			if (myimgmap.areas[id].shape != obj.value && myimgmap.areas[id].shape != 'undefined') {
-				//shape changed, adjust coords intelligently inside _normCoords
-				var coords = '';
-				if (props[id]) {
-					coords  =  props[id].getElementsByTagName('input')[2].value;
-				}
-				else {
-					coords = myimgmap.areas[id].lastInput || '' ;
-				}
-				coords = myimgmap._normCoords(coords, obj.value, 'from'+myimgmap.areas[id].shape);
-				if (props[id]) {
-					props[id].getElementsByTagName('input')[2].value  = coords;
-				}
-				myimgmap.areas[id].shape = obj.value;
-				myimgmap._recalculate(id, coords);
-				myimgmap.areas[id].lastInput = coords;
-			}
-			else if (myimgmap.areas[id].shape == 'undefined') {
-				myimgmap.nextShape = obj.value;
-			}
-		}
-		if (myimgmap.areas[id] && myimgmap.areas[id].shape != 'undefined') {
-			myimgmap._recalculate(id, props[id].getElementsByTagName('input')[2].value);
-			myimgmap.fireEvent('onHtmlChanged', myimgmap.getMapHTML());//temp ## shouldnt be here
-		}
-	}
-
+//	self.myimgmap.setMapHTML(elem.value);
+//	self.myimgmap.removeArea
+//	self.myimgmap.areas[obj.parentNode.aid].lastInput)
+//	self.myimgmap._recalculate(obj.parentNode.aid, obj.value);
 	function ImageMap(context) {
 		var self = this;
 		this.context = context;
+		this.single = false;
+
+		this.myimgmap;
+		this.props = [];
+
+		
+		function gui_row_mouseover(e) {
+			if (self.myimgmap.is_drawing) {return;}//exit if in drawing state
+			if (self.myimgmap.viewmode === 1) {return;}//exit if preview mode
+			var obj = (self.myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
+			if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
+			//console.log(obj.aid);
+			self.myimgmap.highlightArea(obj.aid);
+		}
+
+		function gui_row_mouseout(e) {
+			if (self.myimgmap.is_drawing) {return;}//exit if in drawing state
+			if (self.myimgmap.viewmode === 1) {return;}//exit if preview mode
+			var obj = (self.myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
+			if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
+			self.myimgmap.blurArea(obj.aid);
+		}
+
+		function gui_row_click(e) {
+			if (self.myimgmap.viewmode === 1) {return;}//exit if preview mode
+			var obj = (self.myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
+			if (typeof obj.aid == 'undefined') {obj = obj.parentNode;}
+			gui_row_select(obj.aid, false, false);
+			self.myimgmap.currentid = obj.aid;
+		}
+
+		function gui_row_select(id, setfocus, multiple) {
+			if (self.myimgmap.is_drawing) {return;}//exit if in drawing state
+			if (self.myimgmap.viewmode === 1) {return;}//exit if preview mode
+			if (!document.getElementById('img_active_'+id)) {return;}
+
+			for (var i = 0; i < self.props.length; i++) {
+				if (self.props[i]) {
+					document.getElementById('img_active_'+i).checked = false;
+				}
+			}
+
+			document.getElementById('img_active_'+id).checked = 1;
+			if (setfocus) {
+				document.getElementById('img_active_'+id).focus();
+			}
+			//remove all background styles
+			for (var i = 0; i < self.props.length; i++) {
+				if (self.props[i]) {
+					self.props[i].style.background = '';
+				}
+			}
+			//put highlight on actual props row
+			self.props[id].style.background = '#e7e7e7';
+		}
+
+		function gui_input_change(e) {
+			if (self.myimgmap.viewmode === 1) {return;}//exit if preview mode
+			if (self.myimgmap.is_drawing) {return;}//exit if drawing
+			var obj = (self.myimgmap.isMSIE) ? window.event.srcElement : e.currentTarget;
+			var id = obj.parentNode.aid;
+			if (obj.name == 'img_href')        {self.myimgmap.areas[id].ahref   = obj.value;}
+			else if (obj.name == 'img_alt')    {self.myimgmap.areas[id].aalt    = obj.value;}
+			else if (obj.name == 'img_title')  {self.myimgmap.areas[id].atitle  = obj.value;}
+			else if (obj.name == 'img_target') {self.myimgmap.areas[id].atarget = obj.value;}
+			else if (obj.name == 'img_shape') {
+				if (self.myimgmap.areas[id].shape != obj.value && self.myimgmap.areas[id].shape != 'undefined') {
+					//shape changed, adjust coords intelligently inside _normCoords
+					var coords = '';
+					if (self.props[id]) {
+						coords  =  self.props[id].getElementsByTagName('input')[2].value;
+					}
+					else {
+						coords = self.myimgmap.areas[id].lastInput || '' ;
+					}
+					coords = self.myimgmap._normCoords(coords, obj.value, 'from'+self.myimgmap.areas[id].shape);
+					if (self.props[id]) {
+						self.props[id].getElementsByTagName('input')[2].value  = coords;
+					}
+					self.myimgmap.areas[id].shape = obj.value;
+					self.myimgmap._recalculate(id, coords);
+					self.myimgmap.areas[id].lastInput = coords;
+				}
+				else if (self.myimgmap.areas[id].shape == 'undefined') {
+					self.myimgmap.nextShape = obj.value;
+				}
+			}
+			if (self.myimgmap.areas[id] && self.myimgmap.areas[id].shape != 'undefined') {
+				self.myimgmap._recalculate(id, self.props[id].getElementsByTagName('input')[2].value);
+				self.myimgmap.fireEvent('onHtmlChanged', self.myimgmap.getMapHTML());//temp ## shouldnt be here
+			}
+		}
+
+		
 		
 		this.events = {
 			'.imageMapZoom|change' : function() {
@@ -107,21 +118,21 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 				}
 				pic.width  = pic.oldwidth * scale;
 				pic.height = pic.oldheight * scale;
-				myimgmap.scaleAllAreas(scale);				
+				self.myimgmap.scaleAllAreas(scale);				
 			},
 			'.bounding|click' : function() {
-				myimgmap.config.bounding_box = myimgmap.config.bounding_box ? false : true;
-				myimgmap.relaxAllAreas();
+				self.myimgmap.config.bounding_box = self.myimgmap.config.bounding_box ? false : true;
+				self.myimgmap.relaxAllAreas();
 			},
 			'.labeling|change' : function() {
-				myimgmap.config.label = $(this).val();
-				myimgmap._repaintAll();
+				self.myimgmap.config.label = $(this).val();
+				self.myimgmap._repaintAll();
 			},
 			'.imgmapadd|click' : function() {
-				myimgmap.addNewArea();
+				self.myimgmap.addNewArea();
 			},
 			'.imgmapdel|click' : function() {
-				 myimgmap.removeArea(myimgmap.currentid);
+				 self.myimgmap.removeArea(self.myimgmap.currentid);
 			}
 		};
 		
@@ -133,34 +144,28 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					document.getElementById('html_container').value = str;
 				},
 				onAddArea : function(id)  {
-					//this will reload areas and sets dropdown restrictions
-//					myimgmap.setMapHTML(myimgmap.getMapHTML());
-					
-//					myimgmap.setMapHTML(elem.value);
-//					myimgmap.removeArea
-//					myimgmap.areas[obj.parentNode.aid].lastInput)
-//					myimgmap._recalculate(obj.parentNode.aid, obj.value);
+					if (self.single && id != 0) {
+						return;
+					}
 
-//					props[id].id        = 'img_area_' + id;
-//					props[id].aid       = id;
-//					props[id].className = 'img_area';
+//					self.props[id].id        = 'img_area_' + id;
+//					self.props[id].aid       = id;
+//					self.props[id].className = 'img_area';
 //
 //					$('#form_container', self.context).append(_.template(ImageMapRow, {id : id}));
 //					
 //					return;
 					
-					//var id = props.length;
-					//id = 1;
-					props[id] = document.createElement('DIV');
-					document.getElementById('form_container').appendChild(props[id]);
+					self.props[id] = document.createElement('DIV');
+					document.getElementById('form_container').appendChild(self.props[id]);
 
-					props[id].id        = 'img_area_' + id;
-					props[id].aid       = id;
-					props[id].className = 'img_area';
+					self.props[id].id        = 'img_area_' + id;
+					self.props[id].aid       = id;
+					self.props[id].className = 'img_area';
 					//hook ROW event handlers
-					myimgmap.addEvent(props[id], 'mouseover', gui_row_mouseover);
-					myimgmap.addEvent(props[id], 'mouseout',  gui_row_mouseout);
-					myimgmap.addEvent(props[id], 'click',     gui_row_click);
+					self.myimgmap.addEvent(self.props[id], 'mouseover', gui_row_mouseover);
+					self.myimgmap.addEvent(self.props[id], 'mouseout',  gui_row_mouseout);
+					self.myimgmap.addEvent(self.props[id], 'click',     gui_row_click);
 					var temp = '<input type="text"  name="img_id" class="img_id input-mini" value="' + id + '" readonly="1"/>';
 					//temp+= '<input type="checkbox" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
 					temp+= '<input type="radio" name="img_active" class="img_active" id="img_active_'+id+'" value="'+id+'">';
@@ -171,62 +176,60 @@ function($, _, event, ImageMapTpl, ImageMapRow) {
 					temp+= '<option value="bezier1">bezier</option>';
 					temp+= '</select>';
 					temp+= 'Coords: <input type="text" name="img_coords" class="img_coords" value="">';
-					props[id].innerHTML = temp;
+					self.props[id].innerHTML = temp;
 					//hook more event handlers to individual inputs
 					
-					myimgmap.addEvent(props[id].getElementsByTagName('input')[2],  'change', gui_input_change);
-					myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_input_change);
-					if (myimgmap.isSafari) {
+					self.myimgmap.addEvent(self.props[id].getElementsByTagName('input')[2],  'change', gui_input_change);
+					self.myimgmap.addEvent(self.props[id].getElementsByTagName('select')[0], 'change', gui_input_change);
+					if (self.myimgmap.isSafari) {
 						//need these for safari
-						myimgmap.addEvent(props[id].getElementsByTagName('select')[0], 'change', gui_row_click);
+						self.myimgmap.addEvent(self.props[id].getElementsByTagName('select')[0], 'change', gui_row_click);
 					}
 					//set shape as nextshape if set
-					if (myimgmap.nextShape) {props[id].getElementsByTagName('select')[0].value = myimgmap.nextShape;}
+					if (self.myimgmap.nextShape) {self.props[id].getElementsByTagName('select')[0].value = self.myimgmap.nextShape;}
 					gui_row_select(id, true);					
 				},
 				onRemoveArea : function(id)  {
-					var pprops = props[id].parentNode;
-					pprops.removeChild(props[id]);
+					var pprops = self.props[id].parentNode;
+					pprops.removeChild(self.props[id]);
 					var lastid = pprops.lastChild.aid;
-					props[id] = null;
+					self.props[id] = null;
 					gui_row_select(lastid, true);
-					myimgmap.currentid = lastid;
+					self.myimgmap.currentid = lastid;
 				},
 				onAreaChanged : function(area) {
 					var id = area.aid;
 					if (area.shape) {
-						props[id].getElementsByTagName('select')[0].value = area.shape;
+						self.props[id].getElementsByTagName('select')[0].value = area.shape;
 					}
 					if (area.lastInput) {
-						props[id].getElementsByTagName('input')[2].value  = area.lastInput;
+						self.props[id].getElementsByTagName('input')[2].value  = area.lastInput;
 					}
 				},
 				onSelectArea : function(obj) {
 					gui_row_select(obj.aid, true, false);
 				}
 		};
-		
+
+		this;changeImage = function(url) {
+			self.myimgmap.loadImage(url);
+		};
+		this.display = function() {
+			this.context.append(ImageMapTpl);
+			event.register(this.events, this.context);
+
+			self.myimgmap = new imgmap({
+				mode : "editor",
+				custom_callbacks : this.imgMapCallbacks,
+				pic_container: $('#pic_container', this.context)[0],
+				bounding_box : false
+			});
+			self.myimgmap.strings = imgmapStrings;
+
+			self.myimgmap.loadImage('http://localhost:8080/hcw/hcw/view/admin/imagemap/example1_files/sample1.jpg');				
+		};
+
 	}
-
-	ImageMap.prototype = {
-			changeImage : function(url) {
-				myimgmap.loadImage(url);
-			},
-			display : function() {
-				this.context.append(ImageMapTpl);
-				event.register(this.events, this.context);
-
-				myimgmap = new imgmap({
-					mode : "editor",
-					custom_callbacks : this.imgMapCallbacks,
-					pic_container: $('#pic_container', this.context)[0],
-					bounding_box : false
-				});
-				myimgmap.strings = imgmapStrings;
-
-				myimgmap.loadImage('http://localhost:8080/hcw/hcw/view/admin/imagemap/example1_files/sample1.jpg');				
-			}
-	};
 	
 	return ImageMap;
 });
