6b77a8534271ac4bf11261cb0df81f48fbe937cd
Arnaud Lemaire <dev@norad.fr>
2013-01-25 13:24:54 +0100

    update logback default conf to integrate JUL management and jansi bootstraping

--- server/rest/src/main/java/net/awired/housecream/server/webapi/application/HousecreamContextLoaderListener.java
+++ server/rest/src/main/java/net/awired/housecream/server/webapi/application/HousecreamContextLoaderListener.java
@@ -1,39 +1,15 @@
 package net.awired.housecream.server.webapi.application;
 
 import java.io.InputStream;
-import java.util.logging.Handler;
-import java.util.logging.LogManager;
 import javax.servlet.ServletContextEvent;
 import net.awired.housecream.server.core.application.Housecream;
 import net.awired.housecream.server.core.application.common.HousecreamHome;
-import org.fusesource.jansi.AnsiConsole;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.web.context.ContextLoaderListener;
 
 public class HousecreamContextLoaderListener extends ContextLoaderListener {
 
     @Override
-    public void contextDestroyed(ServletContextEvent event) {
-        // do not uninstall it because it does not reinstall well on hot swap
-        //        AnsiConsole.systemUninstall();
-        SLF4JBridgeHandler.uninstall();
-        super.contextDestroyed(event);
-    }
-
-    @Override
     public void contextInitialized(ServletContextEvent event) {
-
-        AnsiConsole.systemInstall();
-
-        java.util.logging.Logger rootLogger = LogManager.getLogManager().getLogger("");
-        Handler[] handlers = rootLogger.getHandlers();
-        for (Handler handler : handlers) {
-            rootLogger.removeHandler(handler);
-        }
-        SLF4JBridgeHandler.install();
-
-        //
-
         Housecream hc = Housecream.INSTANCE;
         HousecreamHome hcHome = HousecreamHome.INSTANCE;
 
