95ab37a11bfdfda06baa1c26cf332160894138dd
Guillaume Balaine <igosuki@gmail.com>
2013-02-18 00:22:32 +0100

    Added virtual tile size for zoom

--- web/src/main/webapp/js/utils/leaflet-tools.js
+++ web/src/main/webapp/js/utils/leaflet-tools.js
@@ -4,29 +4,32 @@
     LeafMap = (function() {
         function LeafMap(options) {
             if(options == null) options = {};
+            if(options.zoom == null) options.zoom = 1;
 
             var _this = this;
-
             this.image = new Image();
             this.image.src = options.src;
             this.image.onload = function() {
 
-                _this.imageData = {width: this.naturalWidth, height: this.naturalHeight}
-
+                _this.imageData = {width: this.naturalWidth, height: this.naturalHeight};
+                var tileSize = _this.imageData.width/2;
                 _this.map = L.map(options.id, {
                     maxBounds: [[0,0], [_this.imageData.width, _this.imageData.height]],
-                    maxZoom: 26,
-                    center: [_this.imageData.width/2, _this.imageData.height/2]
-                }).setView([0, 0], 13);
+                    zoom: options.zoom,
+                    center: [0, 0]
+                });
 
-                var canvasTiles = L.tileLayer.canvas();
+                var canvasTiles = L.tileLayer.canvas({tileSize: tileSize});
                 canvasTiles.drawTile = function(canvas, tilePoint, zoom) {
                     var ctx = canvas.getContext('2d');
-                    ctx.drawImage(_this.image, 0, 0);
+                    var virtualTileSize = (tileSize/zoom);
+                    var sx = tilePoint.x*virtualTileSize;
+                    var sy = tilePoint.y*virtualTileSize;
+                    ctx.drawImage(_this.image, sx, sy, virtualTileSize, virtualTileSize, 0, 0, tileSize, tileSize);
                     console.log(tilePoint);
                 }
                 canvasTiles.addTo(_this.map);
-                _this.map.addEventListener("zoomstart", function(e) {
+                _this.map.addEventListener("zoomend", function(e) {
                     var currentZoom = e.target._zoom;
                     this.setMaxBounds([[0,0], [_this.imageData.width* currentZoom, _this.imageData.height* currentZoom]]);
                 }, _this.map);
