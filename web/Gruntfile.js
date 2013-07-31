638868d75798df1ff993879f404e5804e856caf0
Arnaud Lemaire <dev@norad.fr>
2013-07-31 11:15:17 +0200

    up

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -321,7 +321,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('server', function (target) {
     if (target === 'dist') {
-      return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
 
     grunt.task.run([
@@ -329,7 +329,6 @@ module.exports = function (grunt) {
       'concurrent:server',
       'configureProxies',
       'connect:livereload',
-      'open',
       'watch'
     ]);
   });
