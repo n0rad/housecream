f948831589b37d87916185e8b7867d3d7fea08e2
Arnaud Lemaire <dev@norad.fr>
2013-09-02 09:24:44 +0200

    fix grunt port

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function (grunt) {
     },
     connect: {
       options: {
-        port: 9000,
+        port: 4500,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost'
       },
